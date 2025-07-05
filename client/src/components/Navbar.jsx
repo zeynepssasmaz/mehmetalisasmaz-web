1bf6db65c8a4f01a06c01d5cce658ff16ab5d0f8
Zeynep Sasmaz <zeynepsasmaz@dev.azure.com>
2025-07-05 22:39:00 +0300

    Home.css düzenlendi, mobil uyum artırıldı

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,17 +1,35 @@
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import './Navbar.css';
 
 function Navbar() {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuOpen(prev => !prev);
+  };
+
+  const closeMenu = () => {
+    setMenuOpen(false);
+  };
+
   return (
     <nav className="navbar">
       <div className="navbar-logo">Mehmet Ali Şaşmaz</div>
-      <ul className="navbar-links">
-        <li><Link to="/">Anasayfa</Link></li>
-        <li><Link to="/neden-biz">Neden Biz</Link></li>
-        <li><Link to="/hizmetler">Hizmetler</Link></li>
-        <li><Link to="/galeri">Galeri</Link></li>
-        <li><Link to="/sss">SSS</Link></li>
-        <li><Link to="/iletisim">İletişim</Link></li>
+
+      <div className={`hamburger ${menuOpen ? 'active' : ''}`} onClick={toggleMenu}>
+        <span></span>
+        <span></span>
+        <span></span>
+      </div>
+
+      <ul className={`navbar-links ${menuOpen ? 'open' : ''}`}>
+        <li><Link to="/" onClick={closeMenu}>Anasayfa</Link></li>
+        <li><Link to="/neden-biz" onClick={closeMenu}>Neden Biz</Link></li>
+        <li><Link to="/hizmetler" onClick={closeMenu}>Hizmetler</Link></li>
+        <li><Link to="/galeri" onClick={closeMenu}>Galeri</Link></li>
+        <li><Link to="/sss" onClick={closeMenu}>SSS</Link></li>
+        <li><Link to="/iletisim" onClick={closeMenu}>İletişim</Link></li>
       </ul>
     </nav>
   );
